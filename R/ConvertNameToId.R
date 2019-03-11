c16815d8d9b042d27fb9408dd23c43af9b67d9a7
Tom Harrop <twharrop@gmail.com>
2019-03-12 10:02:02 +1300

    tidied logic

--- R/ConvertNameToId.R
+++ R/ConvertNameToId.R
@@ -15,17 +15,22 @@
 
 ConvertNameToId <- function(gene_name) {
     id_match <- id_to_name[gene_name, ID]
+    if(!is.na(id_match) && length(id_match) == 1) {
+        return(id_match)
+    }
     if(length(id_match) > 1) {
         warning(paste0("Multiple matches found for ", gene_name))
         warning(print(id_match))
         return(NA)
     }
-    if(!is.na(id_match) && length(id_match) == 1) {
-        return(id_match)
-    }
     alias_match <- id_to_alias[alias == gene_name]
     if(dim(alias_match)[[1]] == 1) {
         return(alias_match[, ID])
+    }
+    if(dim(alias_match)[[1]] > 1) {
+        warning(paste0("Multiple matches found for ", gene_name))
+        warning(print(alias_match))
+        return(NA)
     }
     warning(paste0("No match found for ", gene_name))
     return(NA)
